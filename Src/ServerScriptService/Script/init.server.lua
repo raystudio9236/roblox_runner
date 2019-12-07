5da2cefb7369a965e1a361fe4fa42041271cb96b
RayStudio9236 <raystudio9236@gmail.com>
2019-12-07 16:21:48 +0800

    update

--- Src/ServerScriptService/Script/init.server.lua
+++ Src/ServerScriptService/Script/init.server.lua
@@ -2,5 +2,7 @@ local PlayerManager = require(game.ReplicatedStorage.Source.PlayerManager)
 
 game.ReplicatedStorage.Events.StartRunning.OnServerEvent:connect(function(player)
     print('On Start Running')
+    wait(3)
+
     PlayerManager:Run(player)
 end)
--- Src/StarterPlayerScripts/ControlScript/init.client.lua
+++ Src/StarterPlayerScripts/ControlScript/init.client.lua
@@ -5,10 +5,18 @@ repeat
 until player.Character
 
 local controlsEnabled = false
+local needJump = false
+local needLeft = false
+local needRight = false
+
+local moveDir = Vector3.new(0, 0, -1)
+local leftDir = Vector3.new(-1, 0, -1)
+local rightDir = Vector3.new(1, 0, -1)
 
 game.ReplicatedStorage.Events.EnableControls.OnClientEvent:connect(function(enabled)
 	controlsEnabled = enabled
 end)
+
 
 game:GetService('RunService'):BindToRenderStep(
     'Controls',
@@ -16,8 +24,18 @@ game:GetService('RunService'):BindToRenderStep(
     function()
         if controlsEnabled then
             if player.Character and player.Character:FindFirstChild('Humanoid') then
-                local moveDir = Vector3.new(0, 0, -1)
-                player.Character.Humanoid:Move(moveDir, false)
+                if needLeft then
+                    player.Character.Humanoid:Move(leftDir, false)
+                elseif needRight then
+                    player.Character.Humanoid:Move(rightDir, false)
+                else
+                    player.Character.Humanoid:Move(moveDir, false)
+                end
+
+                if needJump then
+                    player.Character.Humanoid.Jump = true
+                    needJump = false
+                end
             end
         end
     end)
@@ -25,3 +43,32 @@ game:GetService('RunService'):BindToRenderStep(
 print('Player Start Running')
 
 game.ReplicatedStorage.Events.StartRunning:FireServer(player)
+
+local ContextActionService = game:GetService("ContextActionService")
+-- Jump Key
+ContextActionService:BindAction('Jump',
+    function(actionName, inputState, inputObject)
+        needJump = true;
+    end, false, 'w', Enum.KeyCode.Space)
+
+-- Left Key
+ContextActionService:BindAction('Left',
+    function(actionName, inputState, inputObject)
+        if inputState == Enum.UserInputState.End then
+            needLeft = false
+        else
+            needLeft = true
+            needRight = false
+        end
+    end, false, 'a')
+
+-- right Key
+ContextActionService:BindAction('Right',
+    function(actionName, inputState, inputObject)
+        if inputState == Enum.UserInputState.End then
+            needRight = false
+        else
+            needRight = true
+            needLeft = false
+        end
+    end, false, 'd')
